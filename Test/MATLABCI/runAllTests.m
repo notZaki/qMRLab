e2583374d3b76a15c8a81318be2a6636f09e23ba
agahkarakuzu <agahkarakuzu@gmail.com>
2019-02-21 10:55:24 -0800

    Make entry root, give relpath w/o extension

--- Test/MATLABCI/runAllTests.m
+++ Test/MATLABCI/runAllTests.m
@@ -1,4 +1,8 @@
-disp(pwd);
+%disp(pwd);
+% /users/mss.system.Ax1P4l/build/Test/MATLABCI is the startup
+% when you pass -sd "Test/MATLABCI". 
+
+
 bootstrapTest;
 res=moxunit_runtests('BatchExample_test.m','-with_coverage','-cover',pwd,'-cover_exclude','*GUI*','-cover_json_file','coverage_BatchExample.json');
 assert(~res,'Nope.');
