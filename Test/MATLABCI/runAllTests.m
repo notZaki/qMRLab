ed54ae22fe6d00a5fcae6e0d39edcdd36ae30c34
agahkarakuzu <agahkarakuzu@gmail.com>
2019-02-21 10:39:40 -0800

    Show entry dir

--- Test/MATLABCI/runAllTests.m
+++ Test/MATLABCI/runAllTests.m
@@ -1,3 +1,4 @@
+disp(pwd);
 bootstrapTest;
 res=moxunit_runtests('BatchExample_test.m','-with_coverage','-cover',pwd,'-cover_exclude','*GUI*','-cover_json_file','coverage_BatchExample.json');
 assert(~res,'Nope.');
