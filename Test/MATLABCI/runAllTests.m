2509c0bab7dd783c79c53046a85afe05d159274e
agahkarakuzu <agahkarakuzu@gmail.com>
2019-02-21 11:31:50 -0800

    ¯\_(ツ)_/¯ Ok then

--- Test/MATLABCI/runAllTests.m
+++ Test/MATLABCI/runAllTests.m
@@ -1,8 +1,14 @@
-disp(pwd);
+entry = pwd;
+
+lc = strfind(entry,'/Test');
+rtDir = entry(1:lc-1);
+
 % /users/mss.system.Ax1P4l/build/Test/MATLABCI is the startup
 % when you pass -sd "Test/MATLABCI". 
 
-
-bootstrapTest;
+cd(rtDir);
+disp(['Entered ' pwd]);
+list(ls);
+bootstrapTest; % This should add them all.
 res=moxunit_runtests('BatchExample_test.m','-with_coverage','-cover',pwd,'-cover_exclude','*GUI*','-cover_json_file','coverage_BatchExample.json');
 assert(~res,'Nope.');
