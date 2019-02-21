6395a7c5fb61a60d193824195ba865c23bc98189
agahkarakuzu <agahkarakuzu@gmail.com>
2019-02-21 10:14:53 -0800

    Add simple test
    - Check dir str and see if json returns back

---
+++ Test/MATLABCI/runAllTests.m
@@ -0,0 +1,3 @@
+bootstrapTest;
+res=moxunit_runtests('BatchExample_test.m','-with_coverage','-cover',pwd,'-cover_exclude','*GUI*','-cover_json_file','coverage_BatchExample.json');
+assert(~res,'Nope.');
