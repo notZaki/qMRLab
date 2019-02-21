2d7bec1a72b5294e641f3e945fde44599338d58c
agahkarakuzu <agahkarakuzu@gmail.com>
2019-02-21 11:52:05 -0800

    Give absolute path

--- Test/MATLABCI/runAllTests.m
+++ Test/MATLABCI/runAllTests.m
@@ -3,12 +3,11 @@ entry = pwd;
 lc = strfind(entry,'/Test');
 rtDir = entry(1:lc-1);
 
-% /users/mss.system.Ax1P4l/build/Test/MATLABCI is the startup
-% when you pass -sd "Test/MATLABCI". 
-
 cd(rtDir);
 disp(['Entered ' pwd]);
 
+
+
 bootstrapTest; % This should add them all.
-res=moxunit_runtests('BatchExample_test.m','-with_coverage','-cover',pwd,'-cover_exclude','*GUI*','-cover_json_file','coverage_BatchExample.json');
+res=moxunit_runtests([rtDir filesep 'Test/MoxUnitCompatible/BatchExample_test.m'],'-with_coverage','-cover',pwd,'-cover_exclude','*GUI*','-cover_json_file','coverage_BatchExample.json');
 assert(~res,'Nope.');
